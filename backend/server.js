f512e80415f73d4602e85b04d946621ebd2d0844
Cliff Anderson <snappinmambo@gmail.com>
2025-12-17 09:51:55 -0600

    Created and started Express server and created the dotenv file

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import dotenv from 'dotenv';
+import cors from 'cors';
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 4000;
+
+app.use(express.json());
+app.use(cors());
+
+app.use('/', (req, res) => {
+  res.send('Backend server has started successfully');
+});
+
+app.listen(PORT, () => {
+  console.log(`✅🆗 Server started successfully on http://localhost:${PORT}`);
+});
