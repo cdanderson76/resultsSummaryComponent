7cb1b9e5388be01590f1223a4289df035b75bacf
Cliff Anderson <snappinmambo@gmail.com>
2025-12-18 12:38:44 -0600

    started frontend and built the structure of the app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,23 @@
+
+
+function App() {
+
+  return (
+    <div className="canvas">
+      <div className="main-container">
+        <ResultContainer />
+      </div>
+    </div>
+  )
+}
+
+function ResultContainer() {
+
+  return(
+    <div className="result-container">
+
+    </div>
+  )
+}
+
+export default App
